f0f2cfa44e630e8df203bd610fac7d1553cc7b1c
griminir <viktor.elliot.king@gmail.com>
2024-05-06 14:49:56 +0200

    Add project files.

---
+++ emne-3-guess-the-number/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Guess the number");
+
+Random random = new Random();
+
+int rightNumber = random.Next(1, 101);
+bool rightGuess = false;
+
+while (!rightGuess)
+{
+    myGuess();
+}
+
+void myGuess()
+{
+    Console.WriteLine("Guess a number between 1 and 100?");
+    var imGuessing = Console.ReadLine();
+    int guessedNumber = 0; 
+    bool isaNumber = int.TryParse(imGuessing, out guessedNumber);
+
+    if (rightNumber == guessedNumber && isaNumber)
+    {
+        Console.WriteLine("Correct");
+        rightGuess = true;
+    } else if (rightNumber > guessedNumber && isaNumber)
+    {
+        Console.WriteLine("it's higher");
+    } else if (rightNumber < guessedNumber && isaNumber)
+    {
+        Console.WriteLine("it's lower");
+    } else
+    {
+        Console.WriteLine("please type a number");
+    }
+}
