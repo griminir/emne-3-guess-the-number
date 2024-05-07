7d1809bfb5a5eb17fd25e25e358ae9b269602669
griminir <viktor.elliot.king@gmail.com>
2024-05-07 19:08:49 +0200

    refactored code, commented out code is the old one

--- emne-3-guess-the-number/Program.cs
+++ emne-3-guess-the-number/Program.cs
@@ -3,37 +3,62 @@ using System.Diagnostics.CodeAnalysis;
 
 Console.WriteLine("Guess the number");
 
-Random random = new Random();
+var random = new Random();
 
 int rightNumber = random.Next(1, 101);
-bool rightGuess = false;
+//bool rightGuess = false;
 int count = 0;
 
-while (!rightGuess)
+while (!myGuess())
 {
     myGuess();
-    count++;
+    //count++;
 }
 
-void myGuess()
-{
-    Console.WriteLine("Guess a number between 1 and 100?");
-    var imGuessing = Console.ReadLine();
-    int guessedNumber = 0; 
-    bool isaNumber = int.TryParse(imGuessing, out guessedNumber);
+//void myGuess()
+//{
+//    Console.WriteLine("Guess a number between 1 and 100?");
+//    var imGuessing = Console.ReadLine();
+//    int guessedNumber = 0; 
+//    bool isaNumber = int.TryParse(imGuessing, out guessedNumber);
 
-    if (rightNumber == guessedNumber && isaNumber)
-    {
-        Console.WriteLine($"Correct you used {count + 1} guesses");
-        rightGuess = true;
-    } else if (rightNumber > guessedNumber && isaNumber)
-    {
-        Console.WriteLine("it's higher");
-    } else if (rightNumber < guessedNumber && isaNumber)
-    {
-        Console.WriteLine("it's lower");
-    } else
+//    if (rightNumber == guessedNumber && isaNumber)
+//    {
+//        Console.WriteLine($"Correct you used {count + 1} guesses");
+//        rightGuess = true;
+//    } else if (rightNumber > guessedNumber && isaNumber)
+//    {
+//        Console.WriteLine("it's higher");
+//    } else if (rightNumber < guessedNumber && isaNumber)
+//    {
+//        Console.WriteLine("it's lower");
+//    } else
+//    {
+//        Console.WriteLine("please type a number");
+//    }
+//}
+
+bool myGuess()
+{
+    string guess = "";
+    int guessedNumber = 0;
+
+    Console.WriteLine("Guess a number between 1 and 100?");
+    do
     {
         Console.WriteLine("please type a number");
+        guess = Console.ReadLine();
+        count ++;
+    } while (!int.TryParse(guess, out guessedNumber));
+    
+
+    if (rightNumber == guessedNumber)
+    {
+        Console.WriteLine($"Correct you used {count} guesses");
+        return true;
+    } else
+    {
+        Console.WriteLine((guessedNumber > rightNumber) ? "Your guess was too high" : "Your guess was too low");
+        return false;
     }
 }
